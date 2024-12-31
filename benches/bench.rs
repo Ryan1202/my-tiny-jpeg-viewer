536df60271bf413b0dc33a162649738995dcdeed
Ryan1202 <ryan1202@foxmail.com>
2024-12-31 21:26:19 +0800

    改进IDCT性能

--- benches/bench.rs
+++ benches/bench.rs
@@ -4,14 +4,28 @@ use criterion::{criterion_group, criterion_main, Criterion};
 use my_tiny_jpeg_decoder::get_jpeg_image;
 
 fn bench(c: &mut Criterion) {
-    let mut group = c.benchmark_group("bench");
-    group.bench_function("bench", |b| {
+    let mut group = c.benchmark_group("jpeg_decode_bench");
+    group.bench_function("jpeg_decode", |b| {
+        let iterations = 5;
         b.iter(|| {
-            get_jpeg_image("s11138117.jpg".to_string())
+            get_jpeg_image("Rheinfall 6x17.jpg".to_string());
         })
     });
     group.finish();
 }
 
-criterion_group!(benches, bench,);
+fn bench_idct(c: &mut Criterion) {
+    let mut group = c.benchmark_group("idct_bench");
+    let dct = my_tiny_jpeg_decoder::decode::dct::DCT::new();
+    let mut data = [[1f32; 8]; 8];
+    
+    group.bench_function("idct", |b| {
+        b.iter(|| {
+            dct.idct2d(data);
+        })
+    });
+    group.finish();
+}
+
+criterion_group!(benches, bench, bench_idct);
 criterion_main!(benches);
--- src/decode/dct.rs
+++ src/decode/dct.rs
@@ -2,13 +2,11 @@ use std::f32::consts::PI;
 
 #[repr(align(32))]
 pub struct DCT {
-    pub idct2d_data: [[[[f32; 8]; 8]; 8]; 8],
+    pub idct_data: [[f32; 8]; 8],
 }
 
-fn cc(i: usize, j: usize) -> f32 {
-    if i == 0 && j == 0 {
-        return 1.0 / 2.0;
-    } else if i == 0 || j == 0 {
+fn cc(x: usize) -> f32 {
+    if x == 0 {
         return 1.0 / (2.0 as f32).sqrt();
     } else {
         return 1.0;
@@ -17,81 +15,127 @@ fn cc(i: usize, j: usize) -> f32 {
 
 impl DCT {
     pub fn new() -> DCT {
-        let mut tmp: [[[[f32; 8]; 8]; 8]; 8] = Default::default();
+        let mut output = [[0.0f32; 8]; 8];
         for i in 0..8 {
             for j in 0..8 {
-                for x in 0..8 {
-                    let i_cos = ((2 * i + 1) as f32 * PI / 16.0 * x as f32).cos();
-                    for y in 0..8 {
-                        let j_cos = ((2 * j + 1) as f32 * PI / 16.0 * y as f32).cos();
-                        tmp[i][j][x][y] = cc(x, y) * i_cos * j_cos / 4.0;
-                    }
+                let cos_term = ((2.0 * i as f32 + 1.0) * j as f32 * PI / 16.0).cos();
+                output[i][j] = cc(j) * cos_term / 2.0;
+            }
+        }
+        DCT { idct_data: output }
+    }
+
+    pub fn idct(&self, data: [f32; 8]) -> [f32; 8] {
+        let mut tmp: [f32; 8] = Default::default();
+        for i in 0..8 {
+            for j in 0..8 {
+                tmp[i] += self.idct_data[i][j] * data[j];
+            }
+        }
+        tmp
+    }
+    
+    #[cfg(target_feature = "avx")]
+    pub fn idct2d(&self, data: [[f32; 8]; 8]) -> [[f32; 8]; 8] {
+        use std::arch::x86_64::*;
+        let mut tmp: [[f32; 8]; 8] = Default::default();
+        let mut result: [[f32; 8]; 8] = Default::default();
+
+        for i in 0..8 {
+            for j in 0..8 {
+                unsafe {
+                    // 加载 8 个元素
+                    let row = _mm256_loadu_ps(&data[i][0]); // 从 data 的第 i 行加载 8 个元素
+                    let coeff = _mm256_loadu_ps(&self.idct_data[j][0]); // 加载系数矩阵
+                    let mul = _mm256_mul_ps(row, coeff); // 按元素相乘
+                    // 将累加结果水平相加
+                    let low = _mm256_extractf128_ps(mul, 0);
+                    let high = _mm256_extractf128_ps(mul, 1);
+                    let sum_h = _mm_add_ps(low, high);
+                    let sum_h = _mm_hadd_ps(sum_h, sum_h);
+                    tmp[j][i] = _mm_cvtss_f32(_mm_hadd_ps(sum_h, sum_h));
                 }
             }
         }
-        DCT { idct2d_data: tmp }
-    }
-
-    pub fn idct2d(&self, data: [[f32; 8]; 8]) -> [[f32; 8]; 8] {
-        let mut tmp: [[f32; 8]; 8] = Default::default();
+        
         for i in 0..8 {
             for j in 0..8 {
-                tmp[i][j] = self.fmd_simd(data, i, j);
+                unsafe {
+                    // 加载 8 个元素
+                    let row = _mm256_loadu_ps(&tmp[i][0]); // 从 data 的第 i 行加载 8 个元素
+                    let coeff = _mm256_loadu_ps(&self.idct_data[j][0]); // 加载系数矩阵
+                    let mul = _mm256_mul_ps(row, coeff); // 按元素相乘
+                    // 将累加结果水平相加
+                    let low = _mm256_extractf128_ps(mul, 0);
+                    let high = _mm256_extractf128_ps(mul, 1);
+                    let sum_h = _mm_add_ps(low, high);
+                    let sum_h = _mm_hadd_ps(sum_h, sum_h);
+                    result[j][i] = _mm_cvtss_f32(_mm_hadd_ps(sum_h, sum_h));
+                }
             }
         }
-        tmp
+
+        result
+    }
+    
+    // 自动向量化的代码已经比我写的sse代码好了，就不写sse版本了
+
+    #[cfg(not(target_feature = "avx"))]
+    pub fn idct2d(&self, data: [[f32; 8]; 8]) -> [[f32; 8]; 8] {
+        let mut tmp: [[f32; 8]; 8] = Default::default();
+        let mut result: [[f32; 8]; 8] = Default::default();
+        for i in 0..8 {
+            for j in 0..8 {
+                for k in 0..8 {
+                    tmp[j][i] += self.idct_data[j][k] * data[i][k]; // 计算时将data的行列互换
+                }
+            }
+        }        
+        for i in 0..8 {
+            for j in 0..8 {
+                for k in 0..8 {
+                    result[j][i] += self.idct_data[j][k] * tmp[i][k]; // 计算时将data的行列互换
+                }
+            }
+        }
+        result
     }
 
-    #[cfg(target_feature = "avx")]
-    fn fmd_simd(&self, data: [[f32; 8]; 8], i: usize, j: usize) -> f32 {
-        use std::arch::x86_64::*;
+}
 
-        unsafe {
-            let mut acc = _mm256_set1_ps(0.0);
-            for x in (0..8).step_by(2) {
-                let a = _mm256_load_ps(&self.idct2d_data[i][j][x][0]);
-                let b = _mm256_loadu_ps(&data[x][0]);
-                let c = _mm256_load_ps(&self.idct2d_data[i][j][x + 1][0]);
-                let d = _mm256_loadu_ps(&data[x + 1][0]);
-                acc = _mm256_fmadd_ps(a, b, acc);
-                acc = _mm256_fmadd_ps(c, d, acc);
-            }
-            let mut result = [0.0; 8];
-            acc = _mm256_hadd_ps(acc, acc);
-            acc = _mm256_hadd_ps(acc, acc);
-            _mm256_store_ps(&mut result[0], acc);
-            result[0] + result[4]
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_idct() {
+        let dct = DCT::new();
+        let input = [231.0, -32.0, 48.0, -12.0, 0.0, 0.0, 0.0, 0.0];
+        let expected_output = [83.16255985,78.72226374,69.4820188,59.70970069,59.2857482,75.4908429,102.98820747,124.52532417];
+        let output = dct.idct(input);
+        for i in 0..8 {
+            print!("{}, ", output[i]);
+            assert!((output[i] - expected_output[i]).abs() < 1e-5);
         }
     }
 
-    #[cfg(all(not(target_feature = "avx"), target_feature = "sse2"))]
-    fn fmd_simd(&self, data: [[f32; 8]; 8], i: usize, j: usize) -> f32 {
-        use std::arch::x86_64::*;
-        
-        unsafe {
-            let mut acc = _mm_set1_ps(0.0);
-            let mut result = [0.0; 4];
-            for x in 0..8 {
-                let al = _mm_load_ps(&self.idct2d_data[i][j][x][0]);
-                let ah = _mm_load_ps(&self.idct2d_data[i][j][x][4]);
-                let bl = _mm_loadu_ps(&data[x][0]);
-                let bh = _mm_loadu_ps(&data[x][4]);
-                let sum = _mm_add_ps(_mm_mul_ps(al, bl), _mm_mul_ps(ah, bh));
-                acc = _mm_add_ps(acc, sum);
-            }
-            _mm_storeu_ps(&mut result[0], acc);
-            result[0] + result[1] + result[2] + result[3]
-        }
-    }
-
-    #[cfg(all(not(target_feature = "avx"), not(target_feature = "sse2")))]
-    fn fmd_simd(&self, data: [[f32; 8]; 8], i: usize, j: usize) -> f32 {
-        let mut tmp = 0.0;
-        for x in 0..8 {
-            for y in 0..8 {
-                tmp += self.idct2d_data[i][j][x][y] * data[x][y];
+    #[test]
+    fn test_idct2d() {
+        let dct = DCT::new();
+        let input = [[231.0, -32.0, 48.0, -12.0, 0.0, 0.0, 0.0, 0.0],
+                     [231.0, -32.0, 48.0, -12.0, 0.0, 0.0, 0.0, 0.0],
+                     [231.0, -32.0, 48.0, -12.0, 0.0, 0.0, 0.0, 0.0],
+                     [231.0, -32.0, 48.0, -12.0, 0.0, 0.0, 0.0, 0.0],
+                     [231.0, -32.0, 48.0, -12.0, 0.0, 0.0, 0.0, 0.0],
+                     [231.0, -32.0, 48.0, -12.0, 0.0, 0.0, 0.0, 0.0],
+                     [231.0, -32.0, 48.0, -12.0, 0.0, 0.0, 0.0, 0.0],
+                     [231.0, -32.0, 48.0, -12.0, 0.0, 0.0, 0.0, 0.0]];
+        let output1 = dct.idct2d(input);
+        let output2 = dct.idct2d(input);
+        for i in 0..8 {
+            for j in 0..8 {
+                assert!((output1[i][j] - output2[i][j]).abs() < 0.0002);
             }
         }
-        tmp
     }
 }
